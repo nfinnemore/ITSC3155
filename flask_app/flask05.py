ad69871fca9e44ba516492be33057af49a61b7cd
Nick Finnemore <nfinnemo@uncc.edu>
2022-11-08 13:53:35 -0500

    flask app #5 - edit note function handles post and get

--- flask_app/flask05.py
+++ flask_app/flask05.py
@@ -60,15 +60,26 @@ def new_note():
         a_user = db.session.query(User).filter_by(email='nfinnemo@uncc.edu').one()
         return render_template('new.html', user=a_user)
 
-@app.route('/notes/edit/<note_id>')
+@app.route('/notes/edit/<note_id>', methods=['GET', 'POST'])
 def update_note(note_id):
-    #GET request - show new note form to edit note
-    # retrieve user from database
-    a_user = db.session.query(User).filter_by(email='nfinnemo@uncc.edu').one()
+    #check method used for request
+    if request.method == 'POST':
+        title=request.form['title']
+        text = request.form['noteText']
+        note=db.session.query(Note).filter_by(id=note_id).one()
+        note.title = title
+        note.text = text
+        db.session.add(note)
+        db.session.commit()
+        return redirect(url_for('get_notes'))
+    else:
+        #GET request - show new note form to edit note
+        # retrieve user from database
+        a_user = db.session.query(User).filter_by(email='nfinnemo@uncc.edu').one()
 
-    my_note = db.session.query(Note).filter_by(id=note_id).one()
+        my_note = db.session.query(Note).filter_by(id=note_id).one()
 
-    return render_template('new.html', note=my_note, user=a_user)
+        return render_template('new.html', note=my_note, user=a_user)
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
 
